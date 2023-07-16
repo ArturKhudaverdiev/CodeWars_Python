4a0bc0a6b6c49b0238e70f74a1e38ba948d51a8b
ArturKhudaverdiev <arturuskh@gmail.com>
2023-07-16 17:58:01 +0300

    solution of codewars 8 kuy

---
+++ 7_kuy/Who_is_the_killer.py
@@ -0,0 +1,25 @@
+"""
+Some people have been killed!
+You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+
+Task.
+Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+
+{'James': ['Jacob', 'Bill', 'Lucas'],
+ 'Johnny': ['David', 'Kyle', 'Lucas'],
+ 'Peter': ['Lucy', 'Kyle']}
+and also a list of the names of the dead people:
+
+['Lucas', 'Bill']
+return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill'
+
+"""
+
+#Solution
+def killer(suspect_info, dead):
+    for a in suspect_info:
+        for i in dead:
+            if i not in suspect_info[a]:
+                break
+        else:
+            return a
