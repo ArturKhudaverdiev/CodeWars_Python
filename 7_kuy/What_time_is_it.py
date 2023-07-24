4b2606adfc94bee191bc6f92d23b3229add4c87f
ArturKhudaverdiev <arturuskh@gmail.com>
2023-07-24 17:24:44 +0300

    solution of codewars 8 kuy

---
+++ 7_kuy/What_time_is_it.py
@@ -0,0 +1,26 @@
+"""
+Given a time in AM/PM format as a string, convert it to military (24-hour) time as a string.
+
+Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock
+
+Sample Input: 07:05:45PM Sample Output: 19:05:45
+
+Try not to use built in DateTime libraries.
+
+For more information on military time, check the wiki https://en.wikipedia.org/wiki/24-hour_clock#Military_time
+"""
+
+#Solution
+def get_military_time(time):
+    if "AM" in time:
+        time = time.replace("AM", "")
+        l = time.split(":")
+        if l[0] == "12":
+            l[0] = "00"
+    elif "PM" in time:
+        time = time.replace("PM", "")
+        l = time.split(":")
+        l[0] = str(int(l[0]) + 12)
+        if l[0] == "24":
+            l[0] = "12"
+    return ":".join(l)
