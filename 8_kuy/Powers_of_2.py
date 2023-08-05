b5a487f13c11e23596a2dc6d1222cc5ee68d2c03
ArturKhudaverdiev <arturuskh@gmail.com>
2023-08-05 18:42:43 +0300

    solution of codewars 8 kuy

---
+++ 8_kuy/Powers_of_2.py
@@ -0,0 +1,16 @@
+"""
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+"""
+
+#Solution
+def powers_of_two(n):
+    result = []
+    for i in range(n+1):
+        i = 2 ** i
+        result.append(i)
+    return result
