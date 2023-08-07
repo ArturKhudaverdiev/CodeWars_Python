91342e5f82d9efcc25b12109446e4dd6532bd400
ArturKhudaverdiev <arturuskh@gmail.com>
2023-08-07 16:38:11 +0300

    solution of codewars 8 kuy

---
+++ 8_kuy/Get_Nth_Even_Number.py
@@ -0,0 +1,17 @@
+"""
+Return the Nth Even Number
+
+Example(Input --> Output)
+
+1 --> 0 (the first even number is 0)
+3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+100 --> 198
+1298734 --> 2597466
+"""
+
+#Solution
+def nth_even(n):
+    even = 0
+    for i in range(n-1):
+        even += 2
+    return even
