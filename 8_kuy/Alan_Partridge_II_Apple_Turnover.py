cfea4652b896e452b5bc8ed09431fed27e8c68b3
ArturKhudaverdiev <arturuskh@gmail.com>
2023-08-17 20:09:29 +0300

    solution of codewars 8 kuy

---
+++ 8_kuy/Alan_Partridge_II_Apple_Turnover.py
@@ -0,0 +1,15 @@
+"""
+def apple(x):
+    if int(x) ** 2 > 1000:
+        return "It's hotter than the sun!!"
+    else:
+        return "Help yourself to a honeycomb Yorkie for the glovebox."
+"""
+
+#Solution
+
+def apple(x):
+    if int(x)**2 > 1000:
+        return "It's hotter than the sun!!"
+    else:
+        return "Help yourself to a honeycomb Yorkie for the glovebox."
