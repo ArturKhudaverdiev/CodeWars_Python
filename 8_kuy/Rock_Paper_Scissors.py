7dbd0c55f90166b2da58f8e9a440a92c175785f6
ArturKhudaverdiev <arturuskh@gmail.com>
2023-08-29 16:09:43 +0300

    solution of codewars 8 kuy

--- 8_kuy/Rock_Paper_Scissors.py
+++ 8_kuy/Rock_Paper_Scissors.py
@@ -13,7 +13,7 @@ def rps(p1, p2):
         return "Draw!"
     elif p1 == "scissors" and p2 == "paper":
         return "Player 1 won!"
-    elif p1 == "papper" and p2 == "rock":
+    elif p1 == "paper" and p2 == "rock":
         return "Player 1 won!"
     elif p1 == "rock" and p2 == "scissors":
         return "Player 1 won!"
