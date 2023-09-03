85dcdb5eb6cb30747223dddb6981f1ca726c8507
ArturKhudaverdiev <arturuskh@gmail.com>
2023-09-03 20:53:40 +0300

    solution of codewars 8 kuy

---
+++ 8_kuy/Regex-count_lowercase_letters.py
@@ -0,0 +1,20 @@
+"""
+Your task is simply to count the total number of lowercase letters in a string.
+
+Examples
+"abc" ===> 3
+
+"abcABC123" ===> 3
+
+"abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+"" ===> 0;
+
+"ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+"abcdefghijklmnopqrstuvwxyz" ===> 26
+"""
+
+#Solution
+def lowercase_count(strng):
+    return sum(x.islower() for x in strng)
